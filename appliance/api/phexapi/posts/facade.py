6a7dbdc6b34280fff21877e784d5eb5267e2a8d1
Christian Dein <phex@dein-hosting.de>
2021-12-03 10:09:36 +0100

    Use a facade to access services

---
+++ appliance/api/phexapi/posts/facade.py
@@ -0,0 +1,8 @@
+from phexcore import services
+
+from .controller import PostsService
+
+
+def posts() -> PostsService:
+
+    return services.get("posts")
--- appliance/api/phexapi/posts/routes.py
+++ appliance/api/phexapi/posts/routes.py
@@ -23,15 +23,12 @@ def _get_session_creator() -> sqlalchemy.orm.Session:
 @router.get("")
 async def list_posts(
     session: sqlalchemy.orm.Session = fastapi.Depends(_get_session_creator),
-    grant: phexsec.Grant = fastapi.Depends(oidc_scheme),
     consent: phexsec.Consent = fastapi.Depends(
         oidc_scheme.approve("api", phexsec.Access("list", "Post"))
     ),
 ) -> list[schema.PostObject]:
-    from .controller import PostsService
-
-    controller: PostsService = services.get("posts")
-    return await controller.list(session)
+    from .facade import posts
+    return await posts().list(session)
 
 
 @router.post("", status_code=201)
@@ -42,10 +39,8 @@ async def create_post(
         oidc_scheme.approve("api", phexsec.Access("write", "Post"))
     ),
 ) -> schema.PostObject:
-    from .controller import PostsService
-
-    controller: PostsService = services.get("posts")
-    return await controller.create(session, post)
+    from .facade import posts
+    return await posts().create(session, post)
 
 
 @router.get("/{id}")
@@ -56,10 +51,8 @@ async def read_post(
         oidc_scheme.approve("api", phexsec.Access("read", "Post"))
     ),
 ) -> list[schema.PostObject]:
-    from .controller import PostsService
-
-    controller: PostsService = services.get("posts")
-    return await controller.read(session, id)
+    from .facade import posts
+    return await posts().read(session, id)
 
 
 @router.put("/{id}")
@@ -71,10 +64,8 @@ async def update_post(
         oidc_scheme.approve("api", phexsec.Access("write", "Post"))
     ),
 ) -> list[schema.PostObject]:
-    from .controller import PostsService
-
-    controller: PostsService = services.get("posts")
-    return await controller.update(session, id, post)
+    from .facade import posts
+    return await posts().update(session, id, post)
 
 
 @router.delete("/{id}", status_code=204)
@@ -85,7 +76,5 @@ async def delete_post(
         oidc_scheme.approve("api", phexsec.Access("write", "Post"))
     ),
 ) -> list[schema.PostObject]:
-    from .controller import PostsService
-
-    controller: PostsService = services.get("posts")
-    return await controller.delete(session, id)
+    from .facade import posts
+    return await posts().delete(session, id)
