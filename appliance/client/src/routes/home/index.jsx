459ebd368afd5ec222f949d1f9654bd29fbe1a5c
phex-space <phex@dein-hosting.de>
2021-12-20 15:29:21 +0100

    Cleanup

--- appliance/client/src/routes/home/index.jsx
+++ appliance/client/src/routes/home/index.jsx
@@ -1,33 +1,11 @@
 import React from "react";
-import { useDispatch } from "react-redux";
 
-import { Button, Container } from "@mui/material";
-
-import security from "../../features/security";
+import { Container } from "@mui/material";
 
 function Home(props) {
-    const colors = [
-      "primary",
-      "secondary",
-      "error",
-      "warning",
-      "success",
-      "info",
-    ];
-    const dispatch = useDispatch();
   return (
     <Container sx={{ mt: 3 }}>
-      {colors.map((color) => (
-        <Button
-          key={color}
-          color={color}
-          variant="contained"
-          sx={{ mr: 1 }}
-          onClick={() => dispatch(security.actions.login())}
-        >
-          Klicken
-        </Button>
-      ))}
+
     </Container>
   );
 }
